31489e57d15679fc7297f34cd74ceb24c7285ca2
eliasdev <eliasmuniz.dev@gmail.com>
2022-07-16 21:26:03 -0300

    add users helper

--- server/index.js
+++ server/index.js
@@ -3,6 +3,8 @@ const express = require('express');
 const socketio = require('socket.io'); //https://socket.io/
 const http = require('http');
 
+const { addUser, removeUser, getUser, getUsersInRoom } = require('./users.js') 
+
 const PORT = process.env.PORT || 8000;
 
 const router = require('./router');
@@ -15,22 +17,37 @@ const io = socketio(server,{
         methods: ["GET", "POST"],
         credentials: true
       }
-});
+}); 
 
 // Socket io initialization
 io.on('connection', (socket) => {
-    console.log("We have a new CONNECTION");
 
     socket.on('join',({name, room}, callback)=> {
-        console.log(name, room);
+        const {error, user} = addUser({id: socket.id, name, room});
 
-        const error = true;
+        if(error) return callback(error);
 
+        socket.emit('message', {user:'admin', text: `${user.name}, bienvenido a la sala ${user.room}`});
+        // Socket broadcast sends a message to everyone
+        socket.broadcast.to(user.room).emit('message', {user:'admin', text:`${user.name}, se unió a la sala`})
+
+        socket.join(user.room);
+
+        callback();
     })
+
+    // Expect the event on the backend
+    socket.on('sendMessage', (message, callback) => {
+        const user = getUser(socket.id);
+
+        io.to(user.room).emit('message', {user: user.name, text: message});
+
+        callback();
+    });
 
     socket.on('disconnected', () => {
         console.log("User had left");
-    })
+    });
 });
 
 app.use(router)
---
+++ server/users.js
@@ -0,0 +1,38 @@
+/*
+Helper functions for users
+*/
+
+const users = [];
+
+const addUser = ({id, name, room}) => {
+    // Quit spaces and format text
+    name = name.trim().toLowerCase();
+    room = room.trim().toLowerCase();
+
+    const existingUser = users.find((user) => user.room === room && user.name === name);
+
+    if(existingUser){
+        return {error: 'Ya existe el nombre de usuario. Prueba otro'}
+    }
+
+    const user = { id, name, room};
+
+    users.push(user)
+
+    return { user }
+
+}
+
+const removeUser = (id) => {
+    const index = users.findIndex((user)=> user.id === id);
+
+    if(index !== -1){
+        return users.splice(index, 1)[0];
+    }
+}
+
+const getUser = (id) => users.find((user)=> user.id === id);
+
+const getUsersInRoom = (room) => users.filter((user) => user.room == room);
+
+module.exports = {addUser,removeUser,getUser,getUsersInRoom};
